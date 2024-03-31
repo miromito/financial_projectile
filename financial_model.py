b85ee0e1a52a41ff0bafef67f3b1279cac95d065
rodion <radion.litvinov@90north.health>
2024-03-31 21:09:28 +0600

    fixed current_month_income formula

--- financial_model.py
+++ financial_model.py
@@ -6,35 +6,39 @@ class FinancialModel:
                  monthly_expenses: int,
                  apartment_construction_time: int,
                  savings: int,
-                 rental_income: int,
                  apartment_repair_cost: int,
                  mortgages,
                  black_day_savings_target: int,
                  apartment_cost: int,
                  rent_income_per_apartment: int,
+                 income_tax: float = 0.02,
                  ):
         self.current_age = current_age
         self.target_age = target_age
         self.salary_monthly_income = salary_monthly_income
         self.monthly_expenses = monthly_expenses
+        self.income_tax = income_tax
         self.savings = savings
-        self.rental_income = rental_income
         self.mortgages = mortgages
-        self.apartments_owned = 0
-        self.apartments_repair_time = 0
         self.apartment_cost = apartment_cost
-        self.apartment_first_payment = self.apartment_cost * 0.3
+
         self.apartment_repair_cost = apartment_repair_cost
         self.rent_income_per_apartment = rent_income_per_apartment
         self.black_day_savings = black_day_savings_target
         self.apartment_construction_time = apartment_construction_time
+
         self.monthly_savings_goal = self.salary_monthly_income - self.monthly_expenses
+        self.apartment_first_payment = self.apartment_cost * 0.3
         self.months_until_target_age = (self.target_age - self.current_age) * 12
+
+        self.rental_income = 0
+        self.apartments_owned = 0
+        self.apartments_repair_time = 0
 
     def simulate(self):
         for month in range(self.months_until_target_age):
             self.update_rental_income()
-            self.calculate_monthly_finances(month)
+            self.calculate_monthly_finances()
             self.process_mortgages()
             self.check_for_new_apartment()
             self.print_financial_status(month)
@@ -47,9 +51,10 @@ class FinancialModel:
                 self.rental_income += self.rent_income_per_apartment
                 self.apartments_repair_time = 0
 
-    def calculate_monthly_finances(self, month):
-        current_month_income = (self.salary_monthly_income - self.salary_monthly_income * 0.1) + self.rental_income
-        month_savings = current_month_income - self.monthly_expenses
+    def calculate_monthly_finances(self):
+        self.current_month_income = ((self.salary_monthly_income - self.salary_monthly_income * self.income_tax)
+                                + self.rental_income)
+        month_savings = self.current_month_income - self.monthly_expenses
         self.savings += month_savings
 
     def process_mortgages(self):
@@ -74,18 +79,19 @@ class FinancialModel:
     def purchase_apartment(self, cash_purchase=False):
         if cash_purchase:
             self.savings -= self.apartment_cost
-            self.mortgages.append({'amount': 0, 'paid': True, "month_to_complete": 48})
+            self.mortgages.append({'amount': 0, 'paid': True, "month_to_complete": self.apartment_construction_time})
         else:
             self.savings -= (self.apartment_first_payment + self.apartment_repair_cost)
             self.apartments_repair_time = 1
             self.mortgages.append(
-                {'amount': self.apartment_cost - self.apartment_first_payment, 'paid': False, "month_to_complete": 48})
+                {'amount': self.apartment_cost - self.apartment_first_payment, 'paid': False,
+                 "month_to_complete": self.apartment_construction_time})
 
     def print_financial_status(self, month):
         print(
             f'Age: {self.current_age + month // 12}, '
             f'Savings: {self.savings}, '
-            f'income: {(self.salary_monthly_income - self.salary_monthly_income * 0.1) + self.rental_income}, '
+            f'income: {self.current_month_income} '
             f'Rental Income: {self.rental_income}, '
             f'Apartments constructed owned: {len([x for x in self.mortgages if x["paid"] and x["month_to_complete"] <= 0])}, '
             f'Apartments in construction owned: {len([x for x in self.mortgages if x["paid"] and x["month_to_complete"] > 0])}, '
--- main.py
+++ main.py
@@ -3,17 +3,22 @@ from financial_model import FinancialModel
 # Updated initial parameters based on the new script modifications
 current_age = 33
 target_age = 55
-salary_monthly_income = 5300
-monthly_expenses = 1700
-apartment_construction_time_months = 48
+
+salary_monthly_income = 2500
+monthly_expenses = 600
+
 savings = 0
 rental_income = 0
 black_day_savings_target = 50000
+
+apartment_construction_time_months = 48
 rent_income_per_apartment = 500
 apartment_cost = 48000
 apartment_repair_cost = 12000
 
-mortgages = [{'amount': 22650, 'paid': False, "month_to_complete": 36}]
+mortgages = [
+    # {'amount': 22650, 'paid': False, "month_to_complete": 36}
+]
 
 financial_plan = FinancialModel(
     current_age=current_age,
@@ -22,7 +27,6 @@ financial_plan = FinancialModel(
     monthly_expenses=monthly_expenses,
     savings=savings,
     apartment_construction_time=apartment_construction_time_months,
-    rental_income=rental_income,
     mortgages=mortgages,
     black_day_savings_target=black_day_savings_target,
     rent_income_per_apartment=rent_income_per_apartment,
